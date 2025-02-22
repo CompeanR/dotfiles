6c7ba7dea9aeb1d211f9f6958c1d9f70daf241c8
Javier Compean <javiercompean@Javiers-Mac-mini.local>
2025-02-22 10:30:51 -0600

    `shortcuts`: Firefox aerospace and move lines around using KJ in V mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,7 +12,8 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- Select All
 keymap.set("n", "<c-a>", "gg<S-v>G", { desc = "Select All" })
 
--- Duplicated Keymaps
 keymap.del("n", "<leader>db")
 keymap.del("n", "<leader>do")
 keymap.del("n", "<leader>dk")
+keymap.set("x", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+keymap.set("x", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
