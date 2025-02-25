a61250f648ae1e2e9af16d7fcd5394c94827ab88
Javier Compean <javiercompean@Javiers-Mac-mini.local>
2025-02-25 10:51:08 -0600

    `plugins`: code-companion and autopairs added - keymaps shortcut added - there is a conflict between code-companion and autopairs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,3 +17,15 @@ keymap.del("n", "<leader>do")
 keymap.del("n", "<leader>dk")
 keymap.set("x", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 keymap.set("x", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- keymap.set("n", "<C-n>", function()
+--   if vim.fn.getreg("/") == "" then
+--     vim.cmd("normal! *N") -- Start search if no prior search term
+--   end
+--   vim.api.nvim_feedkeys("cgn", "n", false) -- Triggers 'cgn' properly
+-- end, { noremap = true, silent = true })
+--
+keymap.set("n", "<C-n>", function()
+  vim.cmd("let @/ = '\\<' .. expand('<cword>') .. '\\>'") -- Set search to the word under cursor
+  vim.api.nvim_feedkeys("cgn", "n", false) -- Change next occurrence
+end, { noremap = true, silent = true })
