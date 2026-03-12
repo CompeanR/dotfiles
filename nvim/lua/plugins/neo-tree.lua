81f8b92678e63385e582a05426542701f29fb67b
Javier Compean <javier.compeanr@gmail.com>
2026-03-12 10:12:55 -0600

    feat(nvim): disable default neo-tree configs
    - We switched to oil.nvim folder tree. We don't want to have 2 folder
    trees
    - Lazyvim by default enables neo-tree. It has to be manually disabled

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+-- local dynamic_width = math.floor(vim.o.columns * 0.20)
+--
+-- return {
+--   "nvim-neo-tree/neo-tree.nvim",
+--   opts = {
+--     window = {
+--       -- auto_expand_width = true,
+--       width = dynamic_width,
+--     },
+--   },
+-- }
+--
+return { { "nvim-neo-tree/neo-tree.nvim", enabled = false } }
